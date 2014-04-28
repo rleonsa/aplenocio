50545b1eb3ce1551d37d2c5940408ed7254abd23
rleonsa <rleonsa@gmail.com>
2014-04-28 09:55:40 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,30 @@
+Prueba
+application = {
+	content = {
+		--width = 1080,
+        --graphicsCompatibility = 1,
+        width = 768,
+        fps = 60,
+        height = 1365,
+        scale = "zoomEven",
+        yAlign = "bottom"
+	},
+
+    
+    -- Push notifications
+
+    notification =
+    {
+        {
+            google = { projectNumber = "1095659006131", },
+        },
+        -- iphone =
+        -- {
+        --     types =
+        --     {
+        --         "badge", "sound", "alert", "newsstand"
+        --     }
+        -- }
+    }
+        
+}
